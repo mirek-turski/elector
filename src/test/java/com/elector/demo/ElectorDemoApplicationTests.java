9f30a210e3b9f27d50240f61a6576a1080df1970
TURSKI , MIROSLAW <miroslaw.turski@servexternos.gruposantander.com>
2021-12-30 17:30:46 +0100

    More meaningful integration test

--- src/test/java/com/elector/demo/ElectorDemoApplicationTests.java
+++ src/test/java/com/elector/demo/ElectorDemoApplicationTests.java
@@ -1,13 +1,24 @@
 package com.elector.demo;
 
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.elector.InstanceInfo;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {"server.port=0", "spring.cloud.kubernetes.enabled=false"})
 class ElectorDemoApplicationTests {
 
+	@Autowired
+	private InstanceInfo selfInfo;
+
 	@Test
-	void contextLoads() {
+	void testSelfInfo() {
+		await().atMost(3000, TimeUnit.MILLISECONDS).until(() -> selfInfo.isActive());
+		assertEquals(1, selfInfo.getOrder());
 	}
 
 }
