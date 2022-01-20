759412cfaf917bc4e16ae82c1d602f519fa33b57
Miroslaw Turski <miroslaw.turski@gmail.com>
2022-01-20 21:16:46 +0000

    Update demo apps: no scan for lib needed

--- elector-demo-kubernetes/src/main/java/com/elector/demo/ElectorDemoKubernetesApplication.java
+++ elector-demo-kubernetes/src/main/java/com/elector/demo/ElectorDemoKubernetesApplication.java
@@ -3,7 +3,7 @@ package com.elector.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.elector"})
+@SpringBootApplication
 public class ElectorDemoKubernetesApplication {
 
   public static void main(String[] args) {
