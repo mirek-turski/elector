1f9c44f14f98111774ac731836a603d19a6bee74
Miroslaw Turski <miroslaw.turski@gmail.com>
2022-10-23 13:29:42 +0100

    Zookeeper service discovery demo application configured

--- README.md
+++ README.md
@@ -7,7 +7,9 @@ For the pool size equal to one, it behaves as simple leader election with one ma
 Bigger pool sizes can be used for services that are dependent on some external factors like limited pool of connections any given service can use.  
 Any instance with an order number assigned becomes active and based on the number may be able to determine the portion of configuration it will use.
 
-## Spring Simple Service Discovery with elector-demo-simple application
+## Demo applications
+
+### Spring Simple Service Discovery with elector-demo-simple application
 
 ```shell
 cd elector-demo-simple
@@ -17,9 +19,9 @@ docker-compose logs
 docker-compose down
 ```
 
-## Kubernetes Service Discovery with elector-demo-kubernetes application
+### Kubernetes Service Discovery with elector-demo-kubernetes application
 
-### Prepare Kubernetes
+#### Prepare Kubernetes (miikube)
 
 ```shell
 cd elector-demo-kubernetes
@@ -28,7 +30,7 @@ kubectl apply -f ./update-default-service-account.yml -n default
 /bin/sh ./kubernetes-deploy.sh
 ```
 
-## Consul Service Discovery with elector-demo-consul application
+### Consul Service Discovery with elector-demo-consul application
 
 ```shell
 cd elector-demo-consul
--- elector-demo-zookeeper/src/main/java/com/elector/demo/ElectorDemoZookeeperApplication.java
+++ elector-demo-zookeeper/src/main/java/com/elector/demo/ElectorDemoZookeeperApplication.java
@@ -1,10 +1,15 @@
 package com.elector.demo;
 
+import com.elector.InstanceInfo;
+import com.elector.InstanceInfo.InstanceInfoBuilder;
+import javax.annotation.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.zookeeper.discovery.ZookeeperDiscoveryProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -16,14 +21,14 @@ public class ElectorDemoZookeeperApplication {
     SpringApplication.run(ElectorDemoZookeeperApplication.class, args);
   }
 
-//  @Bean
-//  public InstanceInfo selfInfo(
-//      InstanceInfoBuilder builder,
-//      @Nullable ConsulDiscoveryProperties discoveryProperties) {
-//    if (discoveryProperties != null) {
-//      builder.id(discoveryProperties.getInstanceId());
-//      builder.host(discoveryProperties.getHostname());
-//    }
-//    return builder.build();
-//  }
+  @Bean
+  public InstanceInfo selfInfo(
+      InstanceInfoBuilder builder,
+      @Nullable ZookeeperDiscoveryProperties zookeeperDiscoveryProperties) {
+    if (zookeeperDiscoveryProperties != null) {
+      builder.id(zookeeperDiscoveryProperties.getInstanceId());
+      builder.host(zookeeperDiscoveryProperties.getInstanceHost());
+    }
+    return builder.build();
+  }
 }
